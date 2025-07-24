08a714a0772a1e6a7e4d71167b6ec04a4ab5a6ed
al1cezhan9 <alice.az.zhang@gmail.com>
2025-07-24 18:03:47 -0400

    fixed firebase config

--- src/App.js
+++ src/App.js
@@ -14,13 +14,12 @@ import { useEffect, useRef, useState } from 'react';
 
 // Your web app's Firebase configuration
 firebase.initializeApp({
-  apiKey: "AIzaSyB9TORPtwkdZcvBND1-artevRDObiWfF3k",
-  authDomain: "superchat-b5eef.firebaseapp.com",
-  projectId: "superchat-b5eef",
-  storageBucket: "superchat-b5eef.firebasestorage.app",
-  messagingSenderId: "135834610246",
-  appId: "1:135834610246:web:6037fb9a37443a8af5f847",
-  measurementId: "G-C5YJGVPWCW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 })
 
 // local variables
